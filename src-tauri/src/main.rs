9a73104d66bda54a539c9f2489a02f6149ec79e4
Michael Jirsa <michael.jirsa@gitlab.anycoders.cz>
2024-02-13 17:19:55 +0100

    Basic functionality

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,100 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::{sync::Mutex, time::Instant};
+
+// use::sysinfo::{System, SystemExt, ProcessExt};
+use sysinfo::System;
+use tauri::{window, CustomMenuItem, Event, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem, UserAttentionType};
+struct AppState {
+  system: Mutex<System>,
+  client_start: Mutex<Option<Instant>>,
+  game_start: Mutex<Option<Instant>>,
+}
+
+fn main() {
+  let state = AppState {
+    system: Mutex::new(System::new()),
+    client_start: Mutex::new(None),
+    game_start: Mutex::new(None),
+  };
+
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(quit)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(hide);
+  let system_tray = SystemTray::new().with_menu(tray_menu);
+
+  tauri::Builder::default()
+    .system_tray(system_tray)
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::LeftClick { tray_id, position, size, .. } => {
+        let window = app.get_window("main").unwrap();
+        window.show().unwrap();
+        window.unminimize().unwrap();
+        window.set_focus().unwrap();
+      }
+      SystemTrayEvent::MenuItemClick {id, ..} => {
+        match id.as_str() {
+          "quit" => {
+            std::process::exit(0);
+          }
+          "hide" => {
+            let window = app.get_window("main").unwrap();
+            window.hide().unwrap();
+          } 
+          _ => {}
+        }
+      }
+      _ => {}
+    })
+    .on_window_event(|event| match event.event() {
+      tauri::WindowEvent::CloseRequested { api, .. } => {
+        event.window().hide().unwrap();
+        api.prevent_close();
+      }
+      _ => {}
+    })
+    .manage(state)
+    .invoke_handler(tauri::generate_handler![print_process_info])
+    .build(tauri::generate_context!())
+    .expect("error while building application")
+    .run(|_app_handle, event| match event {
+      tauri::RunEvent::ExitRequested { api, .. } => {
+        api.prevent_exit();
+      }
+      _ => {}
+    })
+}
+
+#[tauri::command]
+fn print_process_info(state:tauri::State<'_,AppState>){
+  let mut system = state.system.lock().unwrap();
+  system.refresh_processes();
+  let mut client_start = state.client_start.lock().unwrap();
+  let mut game_start = state.game_start.lock().unwrap();
+  if client_start.is_none() {
+    let runs = system.processes_by_exact_name("LeagueClient.exe").count() > 0;
+    println!("Client runs: {}", runs);
+    if runs {
+      *client_start = Some(Instant::now());
+      println!("Client started.")
+    }
+  } else {
+    let duration = client_start.unwrap().elapsed();
+    println!("Client is running for {:?}", duration);
+  }
+  if game_start.is_none() {
+    let runs = system.processes_by_exact_name("League of Legends.exe").count() > 0;
+    println!("Game runs: {}", runs);
+    if runs {
+      *game_start = Some(Instant::now());
+      println!("Game started.")
+    }
+  }else {
+    let duration = game_start.unwrap().elapsed();
+    println!("Game is running for {:?}", duration);
+  }
+
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import { invoke } from '@tauri-apps/api/tauri'
+    function printProccessInfo(){
+        invoke("print_process_info")
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={printProccessInfo}>Refresh</button>
